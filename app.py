436bcf7de2f1ea309157dd11f74233cdb1e53d3d
Gloria Wekura <wekuraglorian07@gmail.com>
2024-08-31 18:29:05 +0300

    added registration resource

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 from Model.User import db,User
+from resources.user_resource import RegisterResource
 
 
 app = Flask(__name__)
@@ -10,7 +13,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+jwt = JWTManager(app)
+api = Api(app)
 
+api.add_resource(RegisterResource, '/register')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
